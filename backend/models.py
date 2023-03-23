bccc61af28d446711edfb076c111149a73029e9c
riqvade <riqvade@gmail.com>
2023-03-23 23:25:27 +0500

    first Push

---
+++ backend/models.py
@@ -0,0 +1,18 @@
+from exts import db
+
+"""
+class Recipe
+    id:int primary
+    title:str
+    description:str (text)
+
+"""
+
+class Recipe(db.Model):
+    id=db.Column(db.Integer(), primary_key=True)
+    title=db.Column(db.String(), nullable=False)
+    description=db.Column(db.Text(), nullable=False)
+
+    def __repr__(self):
+        return f"<Recipe {self.title} >"
+    
